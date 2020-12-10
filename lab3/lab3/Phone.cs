c30fc241f80c29d4dd79b23bb71570872de9153b
Kirill Pochta <70893878+KirillPochta@users.noreply.github.com>
2020-12-10 12:29:13 +0300

    Add files via upload

---
+++ lab3/lab3/Phone.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+     partial class Phone
+    {
+
+        public int raschet_balanca(ref int add,out int num_2)
+        {
+            
+            num_2 = add+balance;
+
+            return num_2;
+        }
+    }
+    partial class  Phone
+    {
+        
+        int id;
+        private string name;
+        public string Lname;
+        public string adres;
+        public int number_of_card;
+        public int time_of_citys_talks;
+        public int balance;
+        public const int pref_number= +375;
+        public readonly  int year=2020;
+        public static int numbers_of_object=0;
+        public int international_communication;
+        public int a;
+
+        public int privet = 2;
+
+        public static  void info_of_object()
+        {
+            Console.WriteLine("Количество объектов " + numbers_of_object);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Phone phone &&
+                   a == phone.a;
+        }
+
+        public override int GetHashCode()
+        {
+            return -1757793268 + a.GetHashCode();
+        }
+
+        public Phone()
+        {
+            name = "no";
+            Lname = "no";
+            adres = "no";
+            time_of_citys_talks = 0;
+            number_of_card = 0;
+            balance = 0;
+            international_communication=-1;
+            numbers_of_object++;
+        }
+        public Phone(int balance=2)
+        {
+            numbers_of_object++;
+        }
+
+        public Phone(string name,string Lname,string adres,int number_of_card,int time_of_citys_talks,int balance,int pref_number,int international_communication)
+        {
+            this.name = name;
+            this.Lname = Lname;
+            this.adres = adres;
+            this.number_of_card = number_of_card;
+            this.time_of_citys_talks = time_of_citys_talks;
+            this.balance = balance;
+            pref_number = +375;
+            this.international_communication = international_communication;
+            numbers_of_object++;
+        }
+        static Phone()
+        {
+            Console.WriteLine("добавлен новый пользователь");
+        }
+        
+        public string LNAME
+        {
+            get { return this.Lname; }
+            set { this.Lname = value; }
+        }
+        public string ADRES
+        {
+            get { return this.adres; }
+            set { this.adres = value; }
+        }
+        public int Number_of_card
+        {
+            get { return this.number_of_card; }
+            set { this.number_of_card = value; }
+        }
+        public int Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+       
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Id
+        {
+            get//проверка безопастности 
+            {
+                return id;
+            }
+            set
+            {
+                if (Convert.ToBoolean(value)) { id = value; }
+            }
+        }
+
+    }
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone();// new содает объект указанного класса
+            int a = 2;
+            a = 3;
+            phone1.Id = 4;
+            int num = 100;
+            int num_1 = -100;
+            int num_2;
+            Console.WriteLine(phone1.raschet_balanca(ref num,out num_2));
+            Console.WriteLine(phone1.raschet_balanca(ref num_1,out num_2));
+
+            Phone[] phone2_arr = new Phone[3];
+            phone2_arr[0] = new Phone("Кирилл", "Почта", "Ул.Пономаренко", 31231231, 30, 20, +375, 0);
+            phone2_arr[1] = new Phone("Вася", "Мурен", "Ул.Василькова", 2991561, 20, 10, +375, 1);
+            phone2_arr[2] = new Phone("Макс", "Пупсин", "Ул.Васькипа", 31231, 5, 20, +375, 0);
+
+
+           
+            
+       
+            for (int i=0;i<phone2_arr.Length;i++)
+            {
+
+                if (phone2_arr[i].international_communication == 0)
+                    Console.WriteLine("user uses international_communication");
+                if (phone2_arr[i].international_communication == 1)
+                    Console.WriteLine("user does not uses international_communication");
+            } 
+
+
+          
+            for(int i = 0;i<phone2_arr.Length;i++)
+            {
+                phone2_arr[i].Name = "polsovatel "+i;
+                phone2_arr[i].time_of_citys_talks= i ;
+            }
+
+
+
+
+            Console.WriteLine("у этих пользователей нет проблем");
+            for (int i = 0; i < phone2_arr.Length; i++)
+            {
+                if(phone2_arr[i].time_of_citys_talks>1 && phone2_arr[i].time_of_citys_talks<4)
+                {
+                    Console.WriteLine(phone2_arr[i].Name);
+                }
+
+            }
+            Phone.info_of_object();
+        }
+    }
+}
